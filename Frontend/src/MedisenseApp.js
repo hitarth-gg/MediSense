a2fb60b63eabd6edac921cb6e40689e5d67ec5f7
hitarth <goforhitarth@gmail.com>
2024-04-06 05:24:27 +0530

    Added Auth Context + Security

--- Frontend/src/MedisenseApp.js
+++ Frontend/src/MedisenseApp.js
@@ -6,19 +6,37 @@ import Choose from "./pages/Choose";
 import LoginPage from "./pages/LoginPage";
 import DoctorList from "./pages/DoctorList";
 
+import AuthProvider, { useAuth } from "./security/AuthContext";
+
 export default function MedisenseApp() {
+  function AuthenticatedRoute({ children }) {
+    const authContext = useAuth();
+    if (authContext.isAuthenticated) return children;
+    else return <Navigate to="/" />; // Redirect to login page if the user is not authenticated
+  }
+
   return (
     <div className="MedisenseApp">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/choose" element={<Choose />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/list" element={<DoctorList />} />
-          <Route path="*" element={<ErrorComponent />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route
+              path="/choose"
+              element={
+                <AuthenticatedRoute>
+                  <Choose />
+                </AuthenticatedRoute>
+              }
+            />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/list" element={<DoctorList />} />
+            <Route path="*" element={<ErrorComponent />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
